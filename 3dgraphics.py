9ea35c195a9bae91d96a1d39c8fb6db5e4a75394
Thomas <57598094+bmw543@users.noreply.github.com>
2021-07-01 16:43:34 +0200

    Update 3dgraphics.py

--- 3dgraphics.py
+++ 3dgraphics.py
@@ -1,24 +1,10 @@
 import numpy as np
 from numpy import linalg as la
+import math
 import random
 import keyboard
 import pygame
 import time
-
-# screen dimensions
-height = 700
-width = 700
-canvasx = width / height
-
-# movement
-
-mode = 1
-orbit_x = 0
-orbit_y = 0
-distance = 0
-angle_vel = 0.1
-lin_vel = 0.01
-input_refresh = 5
 
 # triangles: vertices 1-3, RGB color
 
@@ -35,80 +21,73 @@ triangles = np.array([[[0, 0, 0], [1, 0, 0], [0, 1, 0], [255, 0, 0]],
                       [[1, 0, 0], [1, 1, 0], [1, 0, 1], [0, 255, 0]],
                       [[1, 1, 1], [1, 1, 0], [1, 0, 1], [0, 255, 0]]])
 
+# screen dimensions
 
+height = 400
+width = 400
+canvasx = width / height
+
+# variables
+
+render_distance = 1000
 
 # classes
 
 class Transformation: # transformation that encodes the camera position
 
     def __init__(self): # initialize as an identity matrix
-        self.angle = 0 # saves angle and computes a new transformation for the angle each time to prevent error accumulation
-        self.matrix = np.array([[1, 0, 0, 0],
-                                [0, 1, 0, 0],
-                                [0, 0, 1, 0],
-                                [0, 0, 0, 1]])
+        self.angles = np.zeros(3)
+        self.rotation, self.translation, self.matrix = np.identity(4), np.identity(4), np.identity(4)
 
-    def rotatex(self, theta, set):
-        if set == True:
-            self.__init__()
-        self.angle += theta
-        sin = np.sin(self.angle)
-        cos = np.cos(self.angle)
-        self.matrix = np.array([[1, 0, 0, 0], # the matrix format for rotating about the x-axis
-                             [0, cos, sin, 0],
-                             [0, -1 * sin, cos, 0],
-                             [0, 0, 0, 1]])
+    def rotate(self, angles):
+        self.angles = angles
+        sin_x = np.sin(self.angles[0])
+        cos_x = np.cos(self.angles[0])
+        sin_y = np.sin(self.angles[1])
+        cos_y = np.cos(self.angles[1])
+        sin_z = np.sin(self.angles[2])
+        cos_z = np.cos(self.angles[2])
+        rotation_x = np.array([[1, 0, 0, 0], # the matrix format for rotating about the x-axis
+                               [0, cos_x, sin_x, 0],
+                               [0, -1 * sin_x, cos_x, 0],
+                               [0, 0, 0, 1]])
+        rotation_y = np.array([[cos_y, 0, -1 * sin_y, 0],
+                               [0, 1, 0, 0],
+                               [sin_y, 0, cos_y, 0],
+                               [0, 0, 0, 1]])
+        rotation_z = np.array([[cos_z, -1 * sin_z, 0, 0],
+                               [sin_z, cos_z, 0, 0],
+                               [0, 0, 1, 0],
+                               [0, 0, 0, 1]])
+        self.rotation = rotation_x.dot(rotation_y).dot(rotation_z)
+        self.matrix = la.inv(self.translation.dot(self.rotation))
 
-    def rotatey(self, theta, set):
-        if set == True:
-            self.__init__()
-        self.angle += theta
-        sin = np.sin(self.angle)
-        cos = np.cos(self.angle)
-        self.matrix = np.array([[cos, 0, -1 * sin, 0],
-                             [0, 1, 0, 0],
-                             [sin, 0, cos, 0],
-                             [0, 0, 0, 1]])
-
-    def rotatez(self, theta, set):
-        if set == True:
-            self.__init__()
-        self.angle += theta
-        sin = np.sin(self.angle)
-        cos = np.cos(self.angle)
-        self.matrix = np.array([[cos, -1 * sin, 0, 0],
-                             [sin, cos, 0, 0],
-                             [0, 0, 1, 0],
-                             [0, 0, 0, 1]])
-
-    def translate(self, x, y, z, set):
-        if set == True:
-            self.__init__()
-        self.matrix[0, 3] += x
-        self.matrix[1, 3] += y
-        self.matrix[2, 3] += z
+    def translate(self, pos):
+        self.translation  = np.array([[1, 0, 0, pos[0]],
+                                      [0, 1, 0, pos[1]],
+                                      [0, 0, 1, pos[2]],
+                                      [0, 0, 0, 1]])
+        self.matrix = la.inv(self.rotation.dot(self.translation))
 
 class Triangle:
 
-    def __init__(self, array):
-        self.points = array
+    def __init__(self, triangle, color):
+        self.vertices = triangle
+        self.transf_vertices = np.zeros(triangle.shape)
+        self.proj_vertices = np.zeros((triangle.shape[0], triangle.shape[1]-2))
+        self.color = color
 
-    def transform(self, matrix):
-        result = np.zeros((3, 3))
+    def transform(self, transformation):
         for i in range(3):
-            result[i] = np.delete(matrix.dot(np.append(self.points[i], [1])), 3)
-
-        return result
+            self.transf_vertices[i] = transformation.matrix.dot(self.vertices[i])
 
     def project(self):
-        result = np.zeros((3, 2))
         for i in range(3):
-            for x in range(2):
-                if self.points[i][2] > -1:
-                    result[0][0] = "nan"
-                else:
-                    result[i][x] = self.points[i][x]/-self.points[i][2]
-        return result
+            if self.transf_vertices[i][2] <= -1:
+                self.proj_vertices[i] = np.divide(self.transf_vertices[i][:3][:2],-self.transf_vertices[i][2])
+            else:
+                return(False)
+        return(True)
 
     def rasterize(self, point, vertices):
         cproducts = [0, 0, 0]
@@ -117,9 +96,9 @@ class Triangle:
             vector2 = np.subtract(vertices[(i+1)%3], vertices[i])
             cproducts[i] = cross(vector1, vector2)
         if abs(sum(cproducts)) == sum(list(map(abs, cproducts))):
-            return 1
+            return True
         else:
-            return 0
+            return False
 
     def barycenter(self, point, vertices):
         coeffs = np.zeros((3, 3))
@@ -135,8 +114,29 @@ class Triangle:
         return result
 
     def z(self, weights):
-        result = weights[0] * self.points[0][2] + weights[1] * self.points[1][2] + weights[2] * self.points[2][2]
+        result = weights[0] * self.transf_vertices[0][2] + weights[1] * self.transf_vertices[1][2] + weights[2] * self.transf_vertices[2][2]
         return result
+
+    def render(self, screen, transformation):
+        self.transform(transformation)
+        if self.project():
+            min_x = math.floor(width*((np.min(self.proj_vertices[:,0])/canvasx)+(1/2)))
+            max_x = math.ceil(width*((np.max(self.proj_vertices[:,0])/canvasx)+(1/2)))
+            min_y = math.floor(height*(1*np.min(self.proj_vertices[:,1])+(1/2)))
+            max_y = math.ceil(height*(1*np.max(self.proj_vertices[:,1])+(1/2)))
+            print(min_x, max_x, min_y, max_y)
+            print(self.proj_vertices[:,1])
+            for x in range(min_x, max_x):
+                for y in range(min_y, max_y):
+                    pixel = np.array([((-0.5 * canvasx) - (0.5 * (canvasx / width)) + (x * canvasx / width)),
+                                      (-0.5 - (0.5 / height) + (y / height))])
+                    if self.rasterize(pixel, self.proj_vertices):
+                        weights = self.barycenter(pixel, self.proj_vertices)
+                        z = self.z(weights)
+                        if z > zmap[x][y]:
+                            draw(screen, (x, y), self.color)
+                            zmap[x][y] = z
+            pygame.display.flip()
 
 # functions
 
@@ -145,9 +145,7 @@ def initscreen():
     screen = pygame.display.set_mode((width, height))
     pygame.display.set_caption("3D")
     screen.fill((0, 0, 0))
-    pixels = list(range(height*width))
-    random.shuffle(pixels)
-    return (screen, pixels)
+    return (screen)
 
 def draw(screen, pixel, color):
     pygame.draw.circle(screen, color, pixel, 0)
@@ -155,145 +153,23 @@ def draw(screen, pixel, color):
 def cross(A, B):
     return (A[0]*B[1]) - (A[1] * B[0])
 
-def input(camera, rotationx, rotationy, rotationz, translation):
-    update = 0
+def run():
+    colors = triangles[:,3]
+    vertices = np.insert(triangles[:,[0,1,2]], 3, 1, axis=2)
+    global zmap
+    zmap = np.full((width, height), float(-1*render_distance))
 
-    if keyboard.is_pressed('w'): # keyboard inputs modify separate matrices for each axis
-        if mode == 'orbit':
-            orbit_x += angle_vel
-            rotationx.rotatex(orbit_x + np.pi, True)
-        rotationx.rotatex(-angle_vel, False)
-        update = 1
+    camera = Transformation()
+    camera.translate((2, 2, 5))
+    camera.rotate((20 * np.pi / 180, -20 * np.pi / 180, 180 * np.pi / 180))
 
-    if keyboard.is_pressed('s'):
-        rotationx.rotatex(angle_vel, False)
-        update = 1
+    screen = initscreen()
 
-    if keyboard.is_pressed('a'):
-        rotationy.rotatey(angle_vel, False)
-        update = 1
-
-    if keyboard.is_pressed('d'):
-        rotationy.rotatey(-angle_vel, False)
-        update = 1
-
-    if keyboard.is_pressed('down_arrow'):
-        y = 0
-        x = np.cos(rotationy.angle + (np.pi / 2)) * lin_vel
-        z = np.sin(rotationy.angle + (np.pi / 2)) * lin_vel
-        translation.translate(x, y, z)
-        update = 1
-
-    if keyboard.is_pressed('up_arrow'):
-        y = 0
-        x = np.cos(rotationy.angle - (np.pi / 2)) * lin_vel
-        z = np.sin(rotationy.angle - (np.pi / 2)) * lin_vel
-        translation.translate(x, y, z)
-        update = 1
-
-    if keyboard.is_pressed('right_arrow'):
-        y = -1 * (np.sin(rotationx.angle) * lin_vel)
-        x = -1 * (np.cos(rotationy.angle) * np.cos(rotationx.angle) * lin_vel)
-        z = -1 * (np.sin(rotationy.angle) * np.cos(rotationx.angle) * lin_vel)
-        translation.translate(x, y, z)
-        update = 1
-
-    if keyboard.is_pressed('left_arrow'):
-        y = np.sin(rotationx.angle) * lin_vel
-        x = np.cos(rotationy.angle) * np.cos(rotationx.angle) * lin_vel
-        z = np.sin(rotationy.angle) * np.cos(rotationx.angle) * lin_vel
-        translation.translate(x, y, z)
-        update = 1
-
-    if update == 1:
-        camera.matrix = np.array([[1, 0, 0, 0],
-                                  [0, 1, 0, 0],
-                                  [0, 0, 1, 0],
-                                  [0, 0, 0, 1]])
-        camera.matrix = camera.matrix.dot(translation.matrix)  # matrices are combined
-        camera.matrix = camera.matrix.dot(rotationy.matrix)
-        camera.matrix = camera.matrix.dot(rotationx.matrix)
-        camera.matrix = camera.matrix.dot(rotationz.matrix)
-
-    return update
-
-def render(screen, camtriangles, projtriangles, x, y):
-    maxz = 0
-    pixel = np.array([((-0.5 * canvasx) - (0.5 * (canvasx / width)) + (x * canvasx / width)),
-                      (-0.5 - (0.5 / height) + (y / height))])
-    for i in range(triangles.shape[0]):
-        triangle = Triangle(camtriangles[i])
-        if triangle.rasterize(pixel, projtriangles[i]) == 1:
-            weights = triangle.barycenter(pixel, projtriangles[i])
-            z = triangle.z(weights)
-            if maxz == 0 or z > maxz:
-                draw(screen, (x, y), triangles[i][3])
-                maxz = z
-    if maxz == 0:
-        draw(screen, (x, y), (0, 0, 0))
-    pygame.display.flip()
-
-def loop(screen, camera, rotationx, rotationy, rotationz, translation, camtriangles, projtriangles, cycle_start, pixels, p):
-    if time.perf_counter() - cycle_start > 1/input_refresh:
-        update = input(camera, rotationx, rotationy, rotationz, translation)
-        if update == 1:
-            screen.fill((0, 0, 0))
-            p = 0
-            inverse = la.inv(camera.matrix)
-
-            camtriangles = np.zeros((triangles.shape[0], triangles.shape[1] - 1, triangles.shape[2]))
-            projtriangles = np.zeros((triangles.shape[0], triangles.shape[1] - 1, triangles.shape[2] - 1))
-
-            for i in range(triangles.shape[0]):
-                triangle = Triangle(np.stack((triangles[i][0], triangles[i][1], triangles[i][2])))
-                camtriangles[i] = triangle.transform(inverse)
-                triangle = Triangle(camtriangles[i])
-                projtriangles[i] = triangle.project()
-
-    if p < width * height:
-        pixel = pixels[p]
-        x, y = pixel % width, int(pixel/width)
-        render(screen, camtriangles, projtriangles, x, y)
-        p += 1
-
-    return camtriangles, projtriangles, p
-
-def run(): # main program that renders the saved triangles
-    done = False
-    rotationx = Transformation() # create the rotation matrices
-    rotationy = Transformation()
-    rotationz = Transformation()
-    translation = Transformation()
-    camera1 = Transformation()
-    rotationz.rotatez(180 * np.pi / 180, False) # set the initial camera position and angle
-    rotationy.rotatey(-20 * np.pi / 180, False)
-    rotationx.rotatex(20 * np.pi / 180, False)
-    translation.translate(2, 2, 5, False)
-    screen, pixels = initscreen()
-    camera1.matrix = camera1.matrix.dot(translation.matrix)  # matrices are combined
-    camera1.matrix = camera1.matrix.dot(rotationy.matrix)
-    camera1.matrix = camera1.matrix.dot(rotationx.matrix)
-    camera1.matrix = camera1.matrix.dot(rotationz.matrix)
-
-    p = 0
-    inverse = la.inv(camera1.matrix)
-    camtriangles = np.zeros((triangles.shape[0], triangles.shape[1] - 1, triangles.shape[2]))
-    projtriangles = np.zeros((triangles.shape[0], triangles.shape[1] - 1, triangles.shape[2] - 1))
-    for i in range(triangles.shape[0]):
-        triangle = Triangle(np.stack((triangles[i][0], triangles[i][1], triangles[i][2])))
-        camtriangles[i] = triangle.transform(inverse)
-        triangle = Triangle(camtriangles[i])
-        projtriangles[i] = triangle.project()
-
-    cycle_start = time.perf_counter()
-
-    while not done:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-        timer = time.perf_counter()
-        camtriangles, projtriangles, p = loop(screen, camera1, rotationx, rotationy, rotationz, translation, camtriangles, projtriangles, cycle_start, pixels, p)
-        print(time.perf_counter() - timer)
-        pygame.display.flip()
-
+    while(True):
+        for i in range(triangles.shape[0]):
+            print(i)
+            triangle = Triangle(vertices[i], triangles[i][3])
+            #print(camera.matrix)
+            #print(triangle.vertices)
+            triangle.render(screen, camera)
 run()
